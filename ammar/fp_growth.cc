208fb1ac741149f36cc04d7ced98b157d2961cfc
Ammar-Sheikh1 <ammarsheikh@sbcglobal.net>
2020-04-30 12:02:22 -0500

    boo

--- ammar/fp_growth.cc
+++ ammar/fp_growth.cc
@@ -35,11 +35,11 @@ void FPGrowth::buildTree(const CSVMatrix<std::string>& data_set) {
   #pragma omp parallel for num_threads(num_threads_)
   for (std::size_t i=0; i<data_set.getRows(); i++) {
     auto item_set = filterLowSupport(data_set.sample(i));
+    omp_set_lock(&lock);
     std::sort(item_set.begin(), item_set.end(), [&](std::string& a, std::string& b){
       return support_map[a] > support_map[b];
     });
     // lock
-    omp_set_lock(&lock);
     tree_.add(item_set);
     omp_unset_lock(&lock);
     // unlock
